fadb5b01314710d1b457f5fa894a9481330212bf
xieboyun-001 <xieboyun-001@cpic.com.cn>
2020-04-12 19:07:10 +0800

    初始化项目

---
+++ src/components/user/user-info/index.vue
@@ -0,0 +1,48 @@
+<template>
+  <div class="user-info">
+		<div class="form-line">
+		  <span class="label">用户名：</span>
+		  <span class="text">{{userForm.username}}</span>
+		</div>
+    <div class="form-line">
+      <span class="label">昵 称：</span>
+      <span class="text">{{userForm.name}}</span>
+    </div>
+    <div class="form-line">
+      <span class="label">电 话：</span>
+      <span class="text">{{userForm.mobile}}</span>
+    </div>
+    <div class="form-line">
+      <span class="label">邮 箱：</span>
+      <span class="text">{{userForm.email}}</span>
+    </div>
+    <a class="btn btn-submit" @click="loadPage('user-center-update')">编辑</a>
+  </div>
+</template>
+<script type="text/ecmascript-6">
+  export default {
+    data() {
+      return {
+        userForm: []
+      };
+    },
+    created() {
+      this.queryUserInfo();
+    },
+    methods: {
+      queryUserInfo() {
+        this.ajax({
+          url: `/uac/user/getInformation`,
+          success: (res) => {
+            if (res.code === '200') {
+              this.userForm = res.data;
+            }
+          }
+        });
+      }
+    },
+    components: {}
+  };
+</script>
+<style rel="stylesheet/scss" lang="scss">
+</style>
---
+++ src/views/about/index.vue
@@ -0,0 +1,37 @@
+<template>
+  <div>
+    <div class="crumb">
+      <div class="w">
+        <div class="crumb-con">
+          <a class="link" href="/">PCMALL</a>
+          <span>></span>
+          <span class="link-text">{{$route.meta.name}}</span>
+        </div>
+      </div>
+    </div>
+    <div class="about-wrap w">
+      <div class="panel">
+        <h1 class="panel-title">关于我们</h1>
+        <div class="panel-body">
+          欢迎访问PCMALL。
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+<script type="text/ecmascript-6">
+  export default {
+    data() {
+      return {};
+    },
+    created() {
+    },
+    methods: {
+      test() {
+      }
+    },
+    components: {}
+  };
+</script>
+<style rel="stylesheet/scss" lang="scss">
+</style>
